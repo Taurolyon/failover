13f626565e663be9d8b4e7f2cb54d22ae3507592
Taurolyon <taurolyon@gmail.com>
2023-03-19 14:06:38 -0400

    Update failover.sh
    Forgot to account for WAN gateway

--- failover.sh
+++ failover.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#   A script by Taurolyon
-#   https://github.com/Taurolyon/failover
 
 PRIMARY_IFACE=enp2s0
 FAILOVER_IFACE=enp3s0
@@ -10,11 +8,14 @@ FAILOVER_CHECK_INTERVAL=300 # check failover connection every 5 minutes
 FAILOVER_FAILURE_THRESHOLD=3 # number of consecutive failures before logging a message
 failedbounce=0
 
+# Get the default gateway from DHCP
+gateway=$(ip route | awk '/^default/ {print $3}')
+
 while true; do
     # Check primary interface for internet connectivity
     if ping -c 1 -I $PRIMARY_IFACE $PING_ADDRESS >/dev/null 2>&1; then
         # Internet connection is up on primary interface, set the default route to the WAN bond interface
-        ip route replace default scope global dev $WAN_BOND_IFACE
+        ip route replace default via $gateway dev $WAN_BOND_IFACE
         logger -t internet-monitor "Switched to $WAN_BOND_IFACE: Internet connection is up on $PRIMARY_IFACE"
         failedbounce=0
         sleep 60
@@ -24,7 +25,7 @@ while true; do
     # Check failover interface for internet connectivity
     if ping -c 1 -I $FAILOVER_IFACE $PING_ADDRESS >/dev/null 2>&1; then
         # Internet connection is up on failover interface, set the default route to the failover interface
-        ip route replace default scope global dev $FAILOVER_IFACE
+        ip route replace default via $gateway dev $FAILOVER_IFACE
         logger -t internet-monitor "Switched to $FAILOVER_IFACE: Internet connection is up on $FAILOVER_IFACE"
         failedbounce=0
         sleep 60
@@ -32,7 +33,7 @@ while true; do
     fi
 
     # Both interfaces are down, set the default route to the failover interface
-    ip route replace default scope global dev $FAILOVER_IFACE
+    ip route replace default via $gateway dev $FAILOVER_IFACE
     logger -t internet-monitor "Switched to $FAILOVER_IFACE: Both $PRIMARY_IFACE and $FAILOVER_IFACE are down"
     failedbounce=0
     sleep 60
