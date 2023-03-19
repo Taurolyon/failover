5120dcd8235504c7a29d5b3f7afadb8f8894e0e2
Taurolyon <taurolyon@gmail.com>
2023-03-19 15:04:27 -0400

    Update failover.sh
    debug output and reconfigure to non-bonded WAN

--- failover.sh
+++ failover.sh
@@ -19,10 +19,12 @@ echo "Gateway: $gateway"
 
 while true; do
     # Check primary interface for internet connectivity
+    echo "Checking internet connectivity on primary interface: $PRIMARY_IFACE"
     if ping -c 1 -I $PRIMARY_IFACE $PING_ADDRESS >/dev/null 2>&1; then
         # Internet connection is up on primary interface, set the default route to the primary interface
         ip route replace default via $gateway dev $PRIMARY_IFACE
         logger -t internet-monitor "Switched to $PRIMARY_IFACE: Internet connection is up on $PRIMARY_IFACE"
+        echo "Switched to $PRIMARY_IFACE: Internet connection is up on $PRIMARY_IFACE"
         failedbounce=0
         sleep 60
         continue
@@ -33,6 +35,7 @@ while true; do
         # Internet connection is up on failover interface, set the default route to the failover interface
         ip route replace default via $gateway dev $FAILOVER_IFACE
         logger -t internet-monitor "Switched to $FAILOVER_IFACE: Internet connection is up on $FAILOVER_IFACE"
+        echo "Switched to $FAILOVER_IFACE: Internet connection is up on $FAILOVER_IFACE"
         failedbounce=0
         sleep 60
         continue
@@ -40,6 +43,7 @@ while true; do
 
     # Both interfaces are down
     logger -t internet-monitor "Both $PRIMARY_IFACE and $FAILOVER_IFACE are down"
+    echo "Both $PRIMARY_IFACE and $FAILOVER_IFACE are down"
     failedbounce=0
     sleep 60
 
@@ -50,11 +54,13 @@ while true; do
             failedbounce=$(( $failedbounce + 1 ))
             if [ $failedbounce -eq $FAILOVER_FAILURE_THRESHOLD ]; then
                 logger -t internet-monitor "Failover interface $FAILOVER_IFACE is down"
+                echo "Failover interface $FAILOVER_IFACE is down"
             fi
         else
             # Failover interface is up, log a message if it was previously down
             if [ $failedbounce -ge $FAILOVER_FAILURE_THRESHOLD ]; then
                 logger -t internet-monitor "Failover interface $FAILOVER_IFACE is up"
+                echo "Failover interface $FAILOVER_IFACE is up"
             fi
             failedbounce=0
         fi
